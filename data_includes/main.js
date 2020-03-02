b743efcb694266de3ee516b528acac338aa8c1f2
PennController <39499242+PennController@users.noreply.github.com>
2020-03-02 11:49:21 -0500

    PennController 1.7

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,41 +1,75 @@
-PennController.ResetPrefix(null);
+// This is a simple demo script, feel free to edit or delete it
+// Find a tutorial and the list of availalbe elements at:
+// https://www.pcibex.net/documentation/
 
-PennController(
-    
-    newImage("logo", "pcibex-logo.png")     // New Image element named "logo" using the file "pcibex-logo.png"
-        .settings.size(150,200)             // Resize it to 150x200px
-        .print()                            // Show the image on the page
+PennController.ResetPrefix(null) // Shorten command names (keep this line here)
+
+// Show the 'intro' trial first, then all the 'experiment' trials in a random order
+// then send the results and finally show the trial labeled 'bye'
+Sequence( "intro", randomize("experiment") , SendResults() , "bye" )
+
+
+// What is in Header happens at the beginning of every single trial
+Header(
+    // We will use this global Var element later to store the participant's name
+    newVar("ParticipantName")
+        .global()
     ,
-    newButton("Start reading")              // New unnamed element named "start" with text "Start reading"
-        .print()                            // Show this element on the page
-        .wait()                             // Wait until the button is clicked
-        .remove()                           // Remove the button
+    // Delay of 250ms before every trial
+    newTimer(250)
+        .start()
+        .wait()
+)
+.log( "Name" , getVar("ParticipantName") )
+// This log command adds a column reporting the participant's name to every line saved to the results
+
+
+newTrial( "intro" ,
+    newImage("pcibex-logo.png")
+        .size( 150 , 200 )      // Resize the image to 150x250px
+        .print()
     ,
-    getImage("logo")                        // Refer back to the Image element named "logo"
-        .remove()                           // Remove it from the page
+    newText("<p>Welcome to the PCIbex demo experiment.</p><p>Please enter your name below and press Enter:</p>")
+        .print()
     ,
-    newText("sentence", "Hello world")      // New Text element named "sentence" with text "Hello world"
-        .print()                            // Show this element on the page
-    ,
-    newTimer(2000)                          // New unnamed Timer element of 2000ms
-        .start()                            // Start the timer
-        .wait()                             // Wait until the timer has ended
-    ,
-    getText("sentence")
-        .remove()                           // Remove the Text element named "sentence" from the page
-    ,
-    newText("<p>What did the sentence say?</p>")
-        .print()                            // Print the question
-    ,
-    newTextInput()                          // New unnamed TextInput element
-        .print()                            // Print the input box
-    ,
-    newText("<p>How confident are you that it is indeed what the sentence said?</p>")
-        .print()                            // Print the question
-    ,
-    newScale(5)                             // New unnamed Scale element with 5 points
-        .settings.slider()                  // Make it a slider
-        .print()                            // Show it
-        .wait()                             // Wait until one of the 5 points is selected
-        
-).setOption("hideProgressBar",true)         // The progress bar should not be visible for this trial
+    newTextInput()
+        .print()
+        .wait()                 // The next command won't be executed until Enter is pressed
+        .setVar( "ParticipantName" )
+        // This setVar command stores the value from the TextInput element into the Var element
+)
+
+
+// This Template command generates as many trials as there are rows in myTable.csv
+Template( "myTable.csv" ,
+    // Row will iteratively point to every row in myTable.csv
+    row => newTrial( "experiment" ,
+        // The trials are minimal: choose a pronoun from a DropDown list
+        newDropDown("pronoun", "...")
+            .before( newText(row.Sentence) )    // Print the sentence to the left of the list
+            .add( row.Pronoun1 )
+            .add( row.Pronoun2 )
+            .shuffle()                          // Randomly order the options in the list (Pronoun1 and Pronoun2)
+            .once()                             // Disable the list after the first selection
+            .print()
+            .wait()
+            .log()                              // Make sure to log the participant's selection
+        ,
+        newButton("Next")
+            .print()
+            .wait()
+    )
+    .log( "Sentence" , row.Sentence )
+    .log( "Pronoun1" , row.Pronoun1 )
+    .log( "Pronoun2" , row.Pronoun2 )
+    // Add these three columns to the results lines of these Template-based trials
+)
+
+
+// Spaces and linebreaks don't matter to the script: we've only been using them for the sake of readability
+newTrial( "bye" ,
+    newText("Thank you for your participation!").print(),
+    newButton().wait()  // Wait for a click on a non-displayed button = wait here forever
+)
+.setOption( "countsForProgressBar" , false )
+// Make sure the progress bar is full upon reaching this last (non-)trial
